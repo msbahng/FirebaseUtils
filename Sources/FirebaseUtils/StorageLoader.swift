0eb4e3157c5ca8373abd2ef20da24321f32598a7
ktc-mooseok <mooseok.bahng@kinto-technologies.com>
2023-11-17 08:35:40 +0900

    modified StorageLoader

--- Sources/FirebaseUtils/StorageLoader.swift
+++ Sources/FirebaseUtils/StorageLoader.swift
@@ -9,24 +9,15 @@ import SwiftUI
 import FirebaseStorage
 import Logger
 
-@Observable
-final public class StorageLoader {
+@Observable final public class StorageLoader {
     
+    public var localUrl: URL?
     public var data: Data?
     public var storageLoaderError: Error?
     
-//    private var cancelableSet = Set<AnyCancellable>()
-//    private let dataPublisher = PassthroughSubject<Data?, Never>()
-    
     public init(storageReferenceUrl: String, id: String) {
-        
-        let url = storageReferenceUrl + "/\(id)"
-        let storage = Storage.storage()
-        let storageRef = storage.reference(forURL: url)
-        
-//        dataPublisher.receive(on: RunLoop.main)
-//            .assign(to: \.data, on: self)
-//            .store(in: &cancelableSet)
+                
+        let storageRef = Self.getStorageReference(storageReferenceUrl: storageReferenceUrl, id: id)
         
         storageRef.getData(maxSize: 1 * 1024 * 1024) { [weak self] data, error in
             if let error = error {
@@ -34,11 +25,39 @@ final public class StorageLoader {
                 self?.storageLoaderError = error
             }
             
-//            DispatchQueue.main.async {
-//                self.dataPublisher.send(data)
-//            }
-            
             self?.data = data
         }
+    }
+    
+    public init(storageReferenceUrl: String, id: String, localBaseUrl: URL) {
+    
+        let localUrl = localBaseUrl.appendingPathComponent(id)
+        
+        guard !FileManager.default.fileExists(atPath: localUrl.path) else {
+            self.localUrl = localUrl
+            return
+        }
+        
+        let storageRef = Self.getStorageReference(storageReferenceUrl: storageReferenceUrl, id: id)
+        
+        storageRef.write(toFile: localUrl) { [weak self] url, error in
+            if let error = error {
+                Logger.printLog("StorageLoader error : \(error)")
+                self?.storageLoaderError = error
+            }
+            
+            self?.localUrl = url
+        }
+    }
+    
+    private static func getStorageReference(
+        storageReferenceUrl: String,
+        id: String
+    ) -> StorageReference {
+        let url = storageReferenceUrl + "/\(id)"
+        let storage = Storage.storage()
+        let storageRef = storage.reference(forURL: url)
+        
+        return storageRef
     }
 }
